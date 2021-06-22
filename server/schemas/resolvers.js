9ea626e5705e7eef9c6282c5d15c144cafb95bda
Janarthani V Subramaniam <vs.janarthani@gmail.com>
2021-06-21 23:20:23 -0700

    added resolvers and updated app.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,67 @@
+const { User } = require ('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (_parent, _args, context) => {
+      if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+              .select('-__v -password');
+
+          return userData;
+      }
+      throw new AuthenticationError('Not logged in!');
+  }
+  },
+  Mutation: {
+    addUser: async (_parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+
+  },
+  login: async (_parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+  },
+  
+  saveBook: async (_parent, { bookData }, context) => {
+      if (context.user) {
+          const updatedUser = await User.findByIdAndUpdate(
+              { _id: context.user._id },
+              { $push: { savedBooks: bookData } },
+              { new: true }
+          );
+          return updatedUser;
+      }
+      throw new AuthenticationError('Not logged in!');
+  },
+  removeBook: async (_parent, { bookId }, context) => {
+      if (context.user) {
+          const updatedUser = await User.findOneAndUpdate(
+              { _id: context.user._id },
+              { $pull: { savedBooks: { bookId } } },
+              { new: true }
+          );
+          return updatedUser;
+      }
+      throw new AuthenticationError('Not logged in!');
+  }
+}
+};
+  
+  module.exports = resolvers;
